9eda8b7e9b1f816c851332c0fd86cfa34ad700bd
Michał Urban <michalurban23@gmail.com>
2017-09-06 14:32:41 +0200

    Create starting application

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import src.controllers.main;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Main mainController = new Main();
+        mainController.start();
+    }
+}
