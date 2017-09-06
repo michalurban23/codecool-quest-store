f834b62f6e25e2af7bd7d2d4a423cef0c5e310d1
Michał Urban <michalurban23@gmail.com>
2017-09-06 14:34:05 +0200

    Add test for constructors or User subclasses

--- src/Test.java
+++ src/Test.java
@@ -6,13 +6,25 @@ public class Test {
 
         User student1 = new Student("Jan", "Kowalski", "qwe", "zxc");
         User student2 = new Student();
-        User student2 = new Student("Stefan", "Batory");
+        User student3 = new Student("Stefan", "Batory");
 
-        Mentor mentor1 = new Mentor("Jaromir", "Jagr", "dupa1", "dupa2");
-        Mentor mentor2 = new Mentor();
-        Mentor mentor3 = new Mentor("George", "Michael", "ohohoho", "uahhaha");
+        User mentor1 = new Mentor("Jaromir", "Jagr", "dupa1", "dupa2");
+        User mentor2 = new Mentor();
+        User mentor3 = new Mentor("George", "Michael", "ohohoho", "uahhaha");
+
+        User admin1 = new Admin("Kazimierz", "Wielki", "drewno", "cegła");
+        User admin2 = new Admin();
 
         System.out.println(student1);
         System.out.println(student1.getFullName());
+        System.out.println(" ");
+
+        System.out.println(student2 + "\n");
+        System.out.println(student3 + "\n");
+        System.out.println(mentor1 + "\n");
+        System.out.println(mentor2 + "\n");
+        System.out.println(mentor3 + "\n");
+        System.out.println(admin1 + "\n");
+        System.out.println(admin2 + "\n");
     }
 }
