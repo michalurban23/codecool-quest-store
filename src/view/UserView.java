ca2f891680b52eec782cd2b713ab878b4f9a2858
smoczis <romanowski.mateusz93@gmail.com>
2017-09-07 17:23:31 +0200

    Rebrand UserView from class into interface

--- src/view/UserView.java
+++ src/view/UserView.java
@@ -1,18 +1,6 @@
-package view;
+public interface UserView{
 
-public class UserView extends ConsoleView{
-
-    public UserView() {;}
-
-    public String[] getStudentInfo(String message){
-
-        System.out.println(message);
-        String[] studentInfo = new String[]{"first name", "last name", "email", "address"};
-        String[] newStudentInfo = new String[4];
-        for(int i = 0; i < 4; i++){
-            newStudentInfo[i] = getString("Type new user's " + studentInfo[i] + ": ");
-        }
-        return newStudentInfo;
-    }
+    public String[] getNewUserData(String message);
+    public void handleMainMenu();
 
 }
