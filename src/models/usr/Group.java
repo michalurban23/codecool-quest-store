a6fca28a20f8d9516a26555dc368c1446dfd69db
Kacper Zelichowski <kafejek@gmail.com>
2017-09-07 11:04:57 +0200

    Implement Group class in models/usr/

--- src/models/usr/Group.java
+++ src/models/usr/Group.java
@@ -1,5 +1,22 @@
 package models.usr;
 
+import java.util.UUID;
+
 public class Group {
-    ;
+  private UUID groupID;
+  private ArrayList<User> usersList;
+
+  public Group(ArrayList<User> usersList) {
+    groupID = UUID.randomUUID();
+    usersList = usersList;
+  }
+
+  private UUID getID() {
+    return groupID;
+  }
+
+  private ArrayList<User> getUsersList() {
+    return usersList;
+  }
+
 }
