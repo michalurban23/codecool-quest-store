1428a7d71ea430f55f201b066189af26aa022825
smoczis <romanowski.mateusz93@gmail.com>
2017-09-07 13:17:36 +0200

    Add UserController template

---
+++ src/controllers/UserController.java
@@ -0,0 +1,28 @@
+public abstract class UserController{
+
+    protected User user;
+    Scanner in;
+
+    public void editUserData(){
+
+        editUserName();
+        editUserSurname();
+        editUserEmail();
+        editUserAddress();
+    }
+
+    public void editUserName(){
+        ;
+    }
+
+    public void editUserSurname(){
+        ;
+    }
+
+    public void editUserEmail(){
+        ;
+    }
+
+    public void editUserAddress(){
+        ;
+    }
