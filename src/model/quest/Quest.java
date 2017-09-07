c96e45f88581998979e185bc9a60102186097ea8
Michał Urban <michalurban23@gmail.com>
2017-09-07 14:30:23 +0200

    Add constructors, setter and getters in quest/

--- src/model/quest/Quest.java
+++ src/model/quest/Quest.java
@@ -1,13 +1,45 @@
 package model.quest;
 
 import java.util.ArrayList;
+import java.util.UUID;
+import java.time.LocalDateTime;
+import model.usr.Holder;
+import model.quest.QuestTemplate.CATEGORY;
 
 public class Quest {
 
-    private static ArrayList<Quest> questList;
+    private Holder owner;
+    private LocalDateTime startTime;
+    private UUID id;
+    private CATEGORY category;
+    private static ArrayList<Quest> questList = new ArrayList<>();
 
-    public Quest() {
+    public Quest(Holder owner) {
 
+        this.id = UUID.randomUUID();
+        this.owner = owner;
+        this.start = LocalDateTime.now();
+        this.category = CATEGORY.BASIC;
+        questList.add(this);
+    }
 
+    public Holder getOwner() {
+
+        return this.owner;
+    }
+
+    public void setOwner(Holder owner) {
+
+        this.owner = owner;
+    }
+
+    public CATEGORY getCategory() {
+
+        return this.category;
+    }
+
+    public void setCategory(Holder owner) {
+
+        this.category = category;
     }
 }
