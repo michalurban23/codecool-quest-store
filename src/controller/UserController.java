793dee8a55d4caaef97f803efa9e21f72826e14a
Michał Urban <michalurban23@gmail.com>
2017-09-08 09:10:17 +0200

    Remove unused methods

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -2,7 +2,7 @@ package controller;
 
 import java.util.Scanner;
 import model.usr.*;
-import view.*;
+import view.ConsoleUserView;
 
 public abstract class UserController {
 
@@ -11,31 +11,24 @@ public abstract class UserController {
 
     public abstract void start(User user);
 
-    // public void setView(ConsoleUserView view) {
-    //     this.view = view;
-    // }
-
     public void setUser(User user) {
+
         this.user = user;
     }
-
-    // public ConsoleUserView getView() {
-    //     return view;
-    // }
 
     public void editUserData(User user) {
         String[] newData = view.getNewUserData();
 
-        if (!(newData[0] == null)) {
+        if (newData[0] != null) {
             user.setFirstName(newData[0]);
         }
-        if (!(newData[1] == null)) {
+        if (newData[1] != null) {
             user.setLastName(newData[1]);
         }
-        if (!(newData[2] == null)) {
+        if (newData[2] != null) {
             user.setEmail(newData[2]);
         }
-        if (!(newData[3] == null)) {
+        if (newData[3] != null) {
             user.setAddress(newData[3]);
         }
     }
