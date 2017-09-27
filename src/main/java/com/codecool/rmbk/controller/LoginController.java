d5c03cb34a9943df4d73040b925f6fb0076df257
Michał Urban <michalurban23@gmail.com>
2017-09-27 10:17:27 +0200

    Add handling wrong password on login screen

--- src/main/java/com/codecool/rmbk/controller/LoginController.java
+++ src/main/java/com/codecool/rmbk/controller/LoginController.java
@@ -10,6 +10,7 @@ public class LoginController {
     private LoginView view;
     private LoginDAO dataAccess;
     private String[] loginInfo;
+    private Boolean appRunning = false;
 
     public LoginController() {
 
@@ -25,22 +26,28 @@ public class LoginController {
         startUserController(loginInfo[0]);
     }
 
-    public void startSQL() {
+    public void startSqlLoginService() {
 
         dataAccess = new SQLLoginDAO();
-        loginInfo = view.LoginScreen();
 
-        if (dataAccess.login(loginInfo)) {
-            startUserController(loginInfo[0]);
+        while (!appRunning) {
+            loginInfo = view.LoginScreen();
+            if (dataAccess.login(loginInfo)) {
+                startUserController(loginInfo[0]);
+            } else {
+                view.showWrongDataMessage();
+            }
         }
     }
 
     private void startUserController(String userLogin) {
 
+        appRunning = true;
         CSVUserInfoDAO userInfo = new CSVUserInfoDAO();
 
         String userType = userInfo.getUserTypeByLogin(userLogin);
         UserController userController = provider.getByUserType(userType);
         userController.start(userInfo.getUserByLogin(userLogin));
     }
+
 }
