879d8a83be652b16d8eb76e9144a0fa46b6121f2
Kacper Zelichowski <kafejek@gmail.com>
2017-09-28 10:57:28 +0200

    Merge conflict

--- src/main/java/com/codecool/rmbk/controller/StudentController.java
+++ src/main/java/com/codecool/rmbk/controller/StudentController.java
@@ -8,43 +8,6 @@ public class StudentController extends UserController {
     }
 
     public StudentController() {
-<<<<<<< HEAD
-
-        view = new ConsoleStudentView();
-        super.view = view;
-    }
-
-    public void start (User user) {
-
-        if (user.getClass().getSimpleName().equals("Student")) {
-            setUser(user);
-            handleStudentMenu();
-        } else {
-            handleSupervisorMenu();
-        }
-    }
-
-    public void handleStudentMenu() {
-
-        boolean isBrowsed = true;
-
-        while (isBrowsed) {
-            view.clearScreen();
-            view.showShortInfo(user);
-            Integer choice = view.handleMainMenu();
-
-            if (choice == 1) {
-                view.showFullInfo(user);
-                editUserData(user);
-            } else if (choice == 2) {
-                ShoppingController shoper = new ShoppingController(user);
-                shoper.startShoppingController();
-            } else if (choice == 0) {
-                isBrowsed = false;
-            }
-        }
-=======
         super();
->>>>>>> dev
     }
 }
