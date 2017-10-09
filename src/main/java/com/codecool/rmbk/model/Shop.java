d8716baedf4498ee5cac78ce15bb594a99fd2f03
Kacper Zelichowski <kafejek@gmail.com>
2017-10-09 13:51:01 +0200

    Delete unused methods from ArtifactController, redirect app flow to the ShoppingController

---
+++ src/main/java/com/codecool/rmbk/model/Shop.java
@@ -0,0 +1,20 @@
+package com.codecool.rmbk.model;
+
+public class Shopping {
+
+    private Cart cart;
+    private Integer id;
+
+    public Shopping(Cart cart, Integer id) {
+        this.cart = cart;
+        this.id = id;
+    }
+
+    public void buyItem() {
+        ;
+    }
+
+    public void checkWallet() {
+        ;
+    }
+}
