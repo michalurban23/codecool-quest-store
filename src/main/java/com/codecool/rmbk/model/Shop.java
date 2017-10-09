18d3f6c9e506224dd0df32cde9ea2da109f143ea
Kacper Zelichowski <kafejek@gmail.com>
2017-10-09 13:52:49 +0200

    Change package name from Shopping to Shop implement cart control methods

--- src/main/java/com/codecool/rmbk/model/Shop.java
+++ src/main/java/com/codecool/rmbk/model/Shop.java
@@ -1,20 +1,42 @@
 package com.codecool.rmbk.model;
 
-public class Shopping {
+import com.codecool.rmbk.model.item.Item;
+
+import java.util.ArrayList;
+
+public class Shop {
 
     private Cart cart;
     private Integer id;
 
-    public Shopping(Cart cart, Integer id) {
+    public Shop(Cart cart, Integer id) {
         this.cart = cart;
         this.id = id;
     }
 
-    public void buyItem() {
+    public void checkWallet() {
         ;
     }
 
-    public void checkWallet() {
-        ;
+    public void addToCart(Item item) {
+        cart.addToCart(item);
+    }
+
+    public ArrayList<Item> getItemsList() {
+        ArrayList<Item> itemsList = cart.getItemsList();
+
+        return itemsList;
+    }
+
+    public void removeFromCart(Item item) {
+        cart.removeFromCart(item);
+    }
+
+    public void flushCart() {
+        cart.flushCart();
+    }
+
+    public Integer getId() {
+        return id;
     }
 }
