331ef50c5f2f6041497e21bb4bb6944150ef26a7
Kacper Zelichowski <kafejek@gmail.com>
2017-10-09 10:27:14 +0200

    Create Shopping.java, implement Shopping class

---
+++ src/main/java/com/codecool/rmbk/model/Shopping.java
@@ -0,0 +1,16 @@
+package com.codecool.rmbk.model;
+
+public class Shopping {
+
+    private Wallet wallet;
+    private Cart cart;
+
+    public Shopping(Wallet wallet, Cart cart) {
+        this.wallet = wallet;
+        this.cart = cart;
+    }
+
+    private void buyItem() {
+        ;
+    }
+}
