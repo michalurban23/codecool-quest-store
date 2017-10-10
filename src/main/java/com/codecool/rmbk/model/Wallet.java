971fb6bfb471dd5e1f65510b7b3269714f4f45a0
Kacper Zelichowski <kafejek@gmail.com>
2017-10-10 13:20:33 +0200

    Implement all neccessary methods in Wallet

--- src/main/java/com/codecool/rmbk/model/Wallet.java
+++ src/main/java/com/codecool/rmbk/model/Wallet.java
@@ -4,15 +4,20 @@ import com.codecool.rmbk.dao.SQLBacklog;
 
 public class Wallet {
 
-    private int coins;
+    private int id;
     private SQLBacklog sqlBacklog = new SQLBacklog();
 
-    public Wallet(int id) {
-        this.coins = sqlBacklog.getCurrentCoins(id);
+    public Wallet(Integer id) {
+        this.id = id;
     }
 
     public int getCoins() {
 
-        return this.coins;
+        return sqlBacklog.getCurrentCoins(id);
+    }
+
+    public void updateWallet(String[] data) {
+
+        sqlBacklog.saveToBacklog(data);
     }
 }
