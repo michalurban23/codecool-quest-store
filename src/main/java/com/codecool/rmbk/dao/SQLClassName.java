092da9d7f4097eaac858b5131be9a7df42661774
smoczis <romanowski.mateusz93@gmail.com>
2017-10-10 11:37:10 +0200

    Add basic sql methods for classes

--- src/main/java/com/codecool/rmbk/dao/SQLClassName.java
+++ src/main/java/com/codecool/rmbk/dao/SQLClassName.java
@@ -1,5 +1,8 @@
 package com.codecool.rmbk.dao;
 
+import com.codecool.rmbk.model.usr.Class;
+import com.codecool.rmbk.model.usr.Mentor;
+
 import java.util.ArrayList;
 
 public class SQLClassName extends SqlDAO {
@@ -12,9 +15,31 @@ public class SQLClassName extends SqlDAO {
         processQuery(query, null);
     }
 
-    public void getClassName(int id) {
+    public String getClassName(int id) {
         String query = "SELECT * FROM class_name WHERE id = ?;";
 
-        processQuery(query, new String[] {"" + id});
+        return processQuery(query, new String[] {"" + id}).get(1).get(1);
     }
+
+    public void updateClassName(Class cls, String newName) {
+
+        String query = "UPDATE class_name SET name = ? WHERE id = ?;";
+        processQuery(query, new String[] {newName, "" + cls.getId()});
+    }
+
+    public void addMentorToClass(Mentor user, Class cls) {
+
+        String query = "UPDATE users SET class_name = ? WHERE id = ?;";
+        processQuery(query, new String[] {cls.getName(), "" + user.getID()});
+    }
+
+    public void removeMentorFromClass(Mentor user) {
+
+        String query = "UPDATE users SET class_name = null where id = ?;";
+        processQuery(query, new String[] {"" + user.getID()});
+    }
+
+    
+
+
 }
