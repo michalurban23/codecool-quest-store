b8e63a268d9e446ca374e352c33eefb719ab829c
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-10-11 14:29:12 +0200

    Add setup method

---
+++ src/test/java/com/codecool/rmbk/model/item/ItemTest.java
@@ -0,0 +1,22 @@
+package com.codecool.rmbk.model.item;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ItemTest {
+
+    private Item item;
+
+    @BeforeEach
+    void setup() {
+        ItemTemplate itemTemplate = mock(ItemTemplate.class);
+        Item item = new Item(itemTemplate, 1);
+    }
+
+
+
+}
