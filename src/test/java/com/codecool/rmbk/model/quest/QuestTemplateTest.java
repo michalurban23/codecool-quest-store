88c2d001ae60c4c0785060c6808ad5ff970796ea
Fesh <maciej.ukowski@gmail.com>
2017-10-12 11:14:54 +0200

    Add QuestTemplateTest with setup and testParameterizedConstuctor1

---
+++ src/test/java/com/codecool/rmbk/model/quest/QuestTemplateTest.java
@@ -0,0 +1,37 @@
+package com.codecool.rmbk.model.quest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestTemplateTest {
+
+    String[] data;
+    ArrayList<String> listData;
+
+    @BeforeEach
+    public void setup() {
+
+    String[] data = {"Explore a dungeon", "Finishing a Teamwork week", "100", "0", "1"};
+
+    ArrayList<String> listData = new ArrayList<>();
+    listData.add("Explore a dungeon");
+    listData.add("Finishing a Teamwork week");
+    listData.add("100");
+    listData.add("0");
+    listData.add("1");
+
+    this.data = data;
+    this.listData = listData;
+    }
+
+    @Test
+    public void testParameterizedConstuctor1() {
+
+        QuestTemplate questTemplate = new QuestTemplate(data);
+        assertNotNull(questTemplate.getName());
+    }
+}
