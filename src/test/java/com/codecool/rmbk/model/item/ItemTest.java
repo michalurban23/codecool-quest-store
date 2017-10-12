97f426d1dd1ac43758953dd79860e6438186c9d2
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-10-12 10:04:51 +0200

    Change test methods and Mockito core

--- src/test/java/com/codecool/rmbk/model/item/ItemTest.java
+++ src/test/java/com/codecool/rmbk/model/item/ItemTest.java
@@ -5,37 +5,39 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class ItemTest {
 
-    private Item item;
-
-    @BeforeEach
-    void setup() {
+    @Test
+    void testCreateItem() {
         ItemTemplate itemTemplate = mock(ItemTemplate.class);
         when(itemTemplate.getSpecial()).thenReturn("1");
         Item item = new Item(itemTemplate, 1);
-    }
-
-    @Test
-    void testCreateItem() {
         assertNotNull(item);
     }
 
     @Test
     void testGetOwner() {
+        ItemTemplate itemTemplate = mock(ItemTemplate.class);
+        when(itemTemplate.getSpecial()).thenReturn("1");
+        Item item = new Item(itemTemplate, 1);
         assertEquals(1, (int) item.getOwner());
     }
 
     @Test
     void testGetTemplate() {
+        ItemTemplate itemTemplate = mock(ItemTemplate.class);
+        when(itemTemplate.getSpecial()).thenReturn("1");
+        Item item = new Item(itemTemplate, 1);
         assertNotNull(item.getTemplate());
     }
 
     @Test
     void testGetBuyTime() {
+        ItemTemplate itemTemplate = mock(ItemTemplate.class);
+        when(itemTemplate.getSpecial()).thenReturn("1");
+        Item item = new Item(itemTemplate, 1);
         assertNotNull(item.getBuyTime());
     }
 
--- src/test/java/com/codecool/rmbk/model/usr/AdminTest.java
+++ src/test/java/com/codecool/rmbk/model/usr/AdminTest.java
@@ -1,5 +1,14 @@
+package com.codecool.rmbk.model.usr;
+
 import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
 
 public class AdminTest extends TestCase {
+
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
 
 }
