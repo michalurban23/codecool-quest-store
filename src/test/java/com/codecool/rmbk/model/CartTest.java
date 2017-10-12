953ac231f2e0e9e60d85c6eb0ce5df4375d92404
Fesh <maciej.ukowski@gmail.com>
2017-10-12 14:02:03 +0200

    Add CartTest class with setup and testAddToCart methods

---
+++ src/test/java/com/codecool/rmbk/model/CartTest.java
@@ -0,0 +1,35 @@
+package com.codecool.rmbk.model;
+
+import com.codecool.rmbk.model.item.Item;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class CartTest {
+
+    Cart cart;
+
+    @Mock
+    Item item;
+
+    @BeforeEach
+    public void setup() {
+
+        Cart cart = new Cart();
+        this.cart = cart;
+
+        Item item = mock(Item.class);
+        this.item = item;
+    }
+
+    @Test
+    public void testAddToCart() {
+
+        cart.addToCart(item);
+
+        assertEquals(1, cart.getItemsList().size());
+    }
+}
