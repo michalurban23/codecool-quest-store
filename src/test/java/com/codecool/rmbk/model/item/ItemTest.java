a846713c9953fffe7affd9d9eed8b35dcd1765b8
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-10-12 11:39:08 +0200

    Refactor - add setup() method

--- src/test/java/com/codecool/rmbk/model/item/ItemTest.java
+++ src/test/java/com/codecool/rmbk/model/item/ItemTest.java
@@ -11,59 +11,52 @@ import static org.mockito.Mockito.*;
 
 class ItemTest {
 
+    private ItemTemplate itemTemplate;
+    private Item item;
+
+    @BeforeEach
+    void setup() {
+        this.itemTemplate = new ItemTemplate("test", "test",
+                "test", "1");
+        this.item = new Item(itemTemplate, 1);
+    }
+
     @Test
     void testCreateItem() {
-        ItemTemplate itemTemplate = mock(ItemTemplate.class);
-        when(itemTemplate.getSpecial()).thenReturn("1");
-        Item item = new Item(itemTemplate, 1);
         assertNotNull(item);
     }
 
     @Test
     void testGetSetOwner() {
-        ItemTemplate itemTemplate = mock(ItemTemplate.class);
-        when(itemTemplate.getSpecial()).thenReturn("1");
-        Item item = new Item(itemTemplate, 1);
         item.setOwner(2);
         assertEquals(2, (int) item.getOwner());
     }
 
     @Test
     void testGetTemplate() {
-        ItemTemplate itemTemplate = mock(ItemTemplate.class);
-        when(itemTemplate.getSpecial()).thenReturn("1");
-        Item item = new Item(itemTemplate, 1);
-        assertNotNull(item.getTemplate());
+        assertEquals(itemTemplate, item.getTemplate());
     }
 
     @Test
     void testGetBuyTime() {
-        ItemTemplate itemTemplate = mock(ItemTemplate.class);
-        when(itemTemplate.getSpecial()).thenReturn("1");
-        Item item = new Item(itemTemplate, 1);
         assertNotNull(item.getBuyTime());
     }
 
     @Test
     void testGetCompletion() {
-        ItemTemplate itemTemplate = mock(ItemTemplate.class);
-        when(itemTemplate.getSpecial()).thenReturn("1");
-        Item item = new Item(itemTemplate, 1);
         assertEquals("30", item.getCompletion());
     }
 
     @Test
     void testSetRightCompletion() {
-        ItemTemplate itemTemplate = mock(ItemTemplate.class);
-        when(itemTemplate.getSpecial()).thenReturn("0");
-        Item item = new Item(itemTemplate, 1);
-        assertEquals("NULL", item.getCompletion());
+        ItemTemplate itemTemplate2 = new ItemTemplate("test", "test",
+                "test", "0");
+        Item item2 = new Item(itemTemplate2, 1);
+        assertEquals("NULL", item2.getCompletion());
     }
 
     @Test
     void testSetRightOwner() {
-        ItemTemplate itemTemplate = mock(ItemTemplate.class);
-        when(itemTemplate.getSpecial()).thenReturn("0");
         assertThrows(IllegalArgumentException.class, () -> new Item(itemTemplate, -1));
     }
 }
