0e7b15c0fbb06d1843da5ac0ff481cff395b4397
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-10-12 12:56:30 +0200

    Add setup method

--- src/test/java/com/codecool/rmbk/model/item/ItemTemplateTest.java
+++ src/test/java/com/codecool/rmbk/model/item/ItemTemplateTest.java
@@ -1,7 +1,16 @@
 package com.codecool.rmbk.model.item;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTemplateTest {
+
+    private ItemTemplate itemTemplate;
+
+    @BeforeEach
+    void setup() {
+        this.itemTemplate = new ItemTemplate("name", "description", "value", "special"));
+    }
 
 }
