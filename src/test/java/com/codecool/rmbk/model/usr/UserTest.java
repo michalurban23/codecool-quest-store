e21a06f79bb0f177ef6f70df8130d48eaca74743
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-10-13 09:53:41 +0200

    create UserTest class

---
+++ src/test/java/com/codecool/rmbk/model/usr/UserTest.java
@@ -0,0 +1,12 @@
+package com.codecool.rmbk.model.usr;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    private static User user;
+
+}
