754cea7a9a9d6516520e5925179fd96bd6b9d737
Kacper Zelichowski <kafejek@gmail.com>
2017-11-08 15:24:43 +0100

    Implement validateUser in QuestWebController

--- src/main/java/com/codecool/rmbk/controller/web/QuestWebController.java
+++ src/main/java/com/codecool/rmbk/controller/web/QuestWebController.java
@@ -1,5 +1,6 @@
 package com.codecool.rmbk.controller.web;
 
+import com.codecool.rmbk.dao.SQLMenuDAO;
 import com.codecool.rmbk.view.WebDisplay;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -9,15 +10,33 @@ import java.io.OutputStream;
 
 public class QuestWebController extends CommonHandler {
 
+    SQLMenuDAO sqlMenuDAO = new SQLMenuDAO();
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
-        String response = WebDisplay.getSiteContent("a", null,
-                null, "templates/mentor_quests.twig");
+        String URL = validateUser();
+
+        String response = WebDisplay.getSiteContent("a", sqlMenuDAO.getSideMenu(),
+                null, URL);
 
         httpExchange.sendResponseHeaders(200, response.getBytes().length);
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
     }
+
+    private String validateUser() {
+
+        String URL = null;
+
+        if (user.getStatus().equals("Mentor")) {
+            URL = "templates/mentor_quests.twig";
+        } else if (user.getStatus().equals("Student")) {
+            URL = "templates/student_quests.twig";
+        }
+
+        return URL;
+    }
+
 }
