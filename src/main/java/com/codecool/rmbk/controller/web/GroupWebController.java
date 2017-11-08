496f982e1a63b0945e7416e6c14350ab5aff117d
Kacper Zelichowski <kafejek@gmail.com>
2017-11-08 13:53:15 +0100

    Create mentor_groups.twig, implemen group and quest web controllers

--- src/main/java/com/codecool/rmbk/controller/web/GroupWebController.java
+++ src/main/java/com/codecool/rmbk/controller/web/GroupWebController.java
@@ -1,10 +1,23 @@
 package com.codecool.rmbk.controller.web;
 
+import com.codecool.rmbk.view.WebDisplay;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-public class GroupWebController implements HttpHandler {
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class GroupWebController extends CommonHandler {
 
     @Override
-    public void handle(HttpExchange httpExchange) {}
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+        String response = WebDisplay.getSiteContent("a", null,
+                null, "templates/mentor_groups.twig");
+
+        httpExchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
 }
