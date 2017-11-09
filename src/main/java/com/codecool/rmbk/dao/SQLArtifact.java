caa1bd3182b0ad45f917e1c9e5a0fc538a9482bb
smoczis <romanowski.mateusz93@gmail.com>
2017-11-09 13:35:28 +0100

    Fix bugs in new methods

--- src/main/java/com/codecool/rmbk/dao/SQLArtifact.java
+++ src/main/java/com/codecool/rmbk/dao/SQLArtifact.java
@@ -34,7 +34,7 @@ public class SQLArtifact extends SqlDAO {
     public Map<String,String> getArtifactMapByUser(User user) {
 
         Map<String,String> result = new HashMap<>();
-        String query = "SELECT `id`, `name` FROM artifacts WHERE `owner` = ?;";
+        String query = "SELECT `id`, `template_name` FROM artifacts WHERE `owner` = ?;";
         ArrayList<ArrayList<String>> queryResult = processQuery(query, new String[] {String.valueOf(user.getID())});
 
         for(ArrayList<String> arr : queryResult.subList(1, queryResult.size())) {
@@ -47,7 +47,7 @@ public class SQLArtifact extends SqlDAO {
     public Map<String,String> getArtifactMapByGroup(Group group) {
 
         Map<String,String> result = new HashMap<>();
-        String query = "SELECT `id`, `name` FROM artifacts WHERE `owner` = ?;";
+        String query = "SELECT `id`, `template_name` FROM artifacts WHERE `owner` = ?;";
         ArrayList<ArrayList<String>> queryResult = processQuery(query, new String[] {String.valueOf(group.getID())});
 
         for(ArrayList<String> arr : queryResult.subList(1, queryResult.size())) {
@@ -56,4 +56,17 @@ public class SQLArtifact extends SqlDAO {
 
         return result;
     }
+
+/*    public <T> Map<String,String> getArtifactMap(T t) {
+
+        Map<String,String> result = new HashMap<>();
+        String query = "SELECT `id`, `template_name` FROM artifacts WHERE `owner` = ?;";
+        ArrayList<ArrayList<String>> queryResult = processQuery(query, new String[] {String.valueOf(t.getID())});
+
+        for(ArrayList<String> arr : queryResult.subList(1, queryResult.size())) {
+            result.put(arr.get(0), arr.get(1));
+        }
+
+        return result;
+    }*/
 }
