208541f7fe14c22cf3ffe9727409e0610fdeb3c4
Michał Urban <michalurban23@gmail.com>
2017-11-23 15:04:27 +0100

    Add working methods for Admin to change exp levels

---
+++ src/main/java/com/codecool/rmbk/controller/web/ExperienceLevelsController.java
@@ -0,0 +1,156 @@
+package com.codecool.rmbk.controller.web;
+
+import com.codecool.rmbk.dao.SQLExperience;
+import com.codecool.rmbk.helper.StringParser;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class ExperienceLevelsController extends CommonHandler {
+
+    private SQLExperience expDao = new SQLExperience();
+    private Map<String, String> request;
+    private String accessLevel;
+    private String name;
+    private String urlExpSlider = "templates/slider.twig";
+    private ArrayList<String> levelData;
+
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+        prepareController(httpExchange);
+        request = parseURIstring(getRequestURI());
+        handleAccessRights();
+    }
+
+    private void prepareController(HttpExchange httpExchange) throws IOException {
+
+        setConnectionData(httpExchange);
+        accessLevel = validateRequest();
+    }
+
+    private void handleAccessRights() throws IOException {
+
+        name = user.getFirstName();
+
+        switch (accessLevel) {
+            case "Admin":
+                handleExperienceLevels();
+                break;
+            default:
+                send403();
+                break;
+        }
+    }
+
+    private void handleExperienceLevels() throws IOException {
+
+        String object = request.get("object");
+        String action = request.get("action");
+
+        if (object == null) {
+            showCurrentLevels();
+        } else if (object.equals("new")) {
+            addLevel();
+        } else {
+            if (action == null) {
+                showLevel(object);
+            } else if (action.equals("remove")) {
+                removeLevel(object);
+            } else if (action.equals("edit")) {
+                editLevel(object);
+            }
+        }
+        send200(response);
+    }
+
+    private void showCurrentLevels() {
+
+        String[] options = {"Add"};
+        Map <String, String> contextMenu = prepareContextMenu(options);
+        Map <String, String> levels = expDao.getLevelsMap();
+
+        response = webDisplay.getSiteContent(name, mainMenu, contextMenu, levels, urlList);
+    }
+
+    private void addLevel() throws IOException {
+
+        String method = httpExchange.getRequestMethod();
+        String title = "Create New Template:";
+        List<String> labels = Arrays.asList("Level Name", "Minimum Required Exp");
+
+        if (method.equals("GET")) {
+            response = webDisplay.getSiteContent(name, mainMenu, null, title, labels, urlAdd);
+        } else if (method.equals("POST")) {
+            readLevelInputs();
+            Boolean properData = verifyInputs();
+            if (properData) {
+                expDao.addNewLevel(levelData);
+                send302("/experience/");
+            } else {
+                showFailureMessage();
+            }
+        }
+    }
+
+    private void showLevel(String object) {
+
+        String[] options = {"Edit", "Remove"};
+
+        Map <String, String> contextMenu = prepareContextMenu(options);
+        Map <String, String> level = expDao.getLevelInfo(object);
+
+        response = webDisplay.getSiteContent(name, mainMenu, contextMenu, level, urlItem);
+    }
+
+    private void editLevel(String object) throws IOException {
+
+        String method = httpExchange.getRequestMethod();
+        String title = "Editing " + StringParser.addWhitespaces(object) + ":";
+        List<String> boundaries = expDao.getBoundaries(object);
+
+        if (method.equals("GET")) {
+            response = webDisplay.getSiteContent(name, mainMenu, null, title, boundaries, urlExpSlider);
+        } else if (method.equals("POST")) {
+            readLevelInputs();
+            expDao.editLevels(object, levelData);
+            send302("/experience/");
+        }
+    }
+
+    private void removeLevel(String object) throws IOException {
+
+        expDao.removeLevel(object);
+        send302("/experience/");
+    }
+
+    private void readLevelInputs() throws IOException {
+
+        Map<String, String> inputs = readInputs();
+        levelData = new ArrayList<>();
+
+        levelData.add(inputs.get("Level Name"));
+        levelData.add(inputs.get("Minimum Required Exp"));
+    }
+
+    private Boolean verifyInputs() {
+
+        try {
+            Integer value = Integer.parseInt(levelData.get(1));
+
+            return value > 0;
+
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private void showFailureMessage() throws IOException {
+
+        send302("/experience"); // TODO
+    }
+
+}
--- src/main/java/com/codecool/rmbk/dao/SQLExperience.java
+++ src/main/java/com/codecool/rmbk/dao/SQLExperience.java
@@ -1,24 +1,101 @@
 package com.codecool.rmbk.dao;
 
-import java.util.ArrayList;
+import com.codecool.rmbk.helper.StringParser;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
 
 public class SQLExperience extends SqlDAO {
 
-    public ArrayList<ArrayList<String>> getExperienceLevels() {
+    public Map<String, String> getLevelsMap() {
 
-        String query = "SELECT * FROM experience";
+        Map<String,String> result = new LinkedHashMap<>();
+
+        String query = "SELECT level, value " +
+                "FROM experience " +
+                "ORDER BY value ASC";
 
         processQuery(query, null);
-        return getResults();
+
+        for (ArrayList<String> outcome : getResults().subList(1, getResults().size())) {
+            String href = "/experience/" + StringParser.removeWhitespaces(outcome.get(0));
+            String name = outcome.get(0);
+            result.put(name, href);
+        }
+        return result;
     }
 
-    public String getExperienceInfo(String level) {
+    public Map<String, String> getLevelInfo(String levelName) {
 
-        String query = "SELECT level FROM experience WHERE value < ? ORDER BY value DESC LIMIT 1;";
+        Map<String,String> result = new HashMap<>();
 
-        processQuery(query, new String[] {level});
+        String query = "SELECT * " +
+                "FROM experience " +
+                "WHERE level = ?;";
+        String[] data = {StringParser.addWhitespaces(levelName)};
 
-        String levelName = getResults().get(1).get(0);
-        return levelName;
+        processQuery(query, data);
+
+        for(int i=0; i<getResults().get(0).size(); i++) {
+            String key = getResults().get(0).get(i);
+            String value = getResults().get(1).get(i);
+            result.put(key, value);
+        }
+        return result;
     }
+
+    public void addNewLevel(List<String> data) {
+
+        String name = StringUtils.capitalize(StringParser.addWhitespaces(data.get(0)));
+        String query = "INSERT INTO experience(level, value) " +
+                "VALUES ('" + name + "', ?)";
+
+        processQuery(query, data.subList(1, data.size()).toArray(new String[0]));
+    }
+
+    public List<String> getBoundaries(String levelName) {
+
+        String name = StringParser.addWhitespaces(levelName);
+        String query = "SELECT value FROM experience " +
+                "WHERE value <= (SELECT value FROM experience " +
+                "WHERE value > (SELECT value FROM experience " +
+                "WHERE level = ?) ORDER BY value ASC LIMIT 1) " +
+                "ORDER BY value DESC " +
+                "LIMIT 3;";
+        String[] data = {name};
+
+        processQuery(query, data);
+
+        List<String> thresholds = new ArrayList<>();
+
+        for (ArrayList<String> outcome : getResults().subList(1, getResults().size())) {
+            thresholds.add(outcome.get(0));
+        }
+        thresholds.add(levelName);
+
+        return thresholds;
+    }
+
+    public void removeLevel(String name) {
+
+        name = StringParser.addWhitespaces(name);
+        String query = "DELETE FROM experience WHERE `level` = ?;";
+
+        processQuery(query, new String[] {name});
+    }
+
+    public void editLevels(String object, ArrayList<String> levelData) {
+
+        String name = StringParser.addWhitespaces(levelData.get(0));
+        String newValue = levelData.get(1);
+        object = StringParser.addWhitespaces(object);
+
+        String query = "UPDATE experience " +
+                "SET `value` = ?, `level` = ? " +
+                "WHERE `level` = '" + object + "';";
+        String[] data = {newValue, name};
+
+        processQuery(query, data);
+    }
+
 }
