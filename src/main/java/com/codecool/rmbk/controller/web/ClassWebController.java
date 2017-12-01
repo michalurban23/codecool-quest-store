cb27d4b95d560bed559c59b64e5d0cc63c1b2ac5
smoczis <romanowski.mateusz93@gmail.com>
2017-12-01 05:56:39 +0100

    Repair classes displaying in both mentor and admin

--- src/main/java/com/codecool/rmbk/controller/web/ClassWebController.java
+++ src/main/java/com/codecool/rmbk/controller/web/ClassWebController.java
@@ -163,6 +163,9 @@ public class ClassWebController extends CommonHandler {
 
         if (loggedUser.getClass().getSimpleName().equals("Student")) {
             send403();
+        } else if (loggedUser.getClass().getSimpleName().equals("Mentor")) {
+            view.setClassDetailsView(object);
+            send200(view.getResponse());
         } else {
             view.setContextMenu(prepareContextMenu(getContextOptions()));
             view.setClassDetailsView(object);
@@ -174,8 +177,10 @@ public class ClassWebController extends CommonHandler {
 
         if (loggedUser.getClass().getSimpleName().equals("Student")) {
             send403();
+        } else if (loggedUser.getClass().getSimpleName().equals("Mentor")) {
+            view.setClassListMentorView(sqlClass.getKlassURLMap());
+            send200(view.getResponse());
         } else {
-            view.setContextMenu(prepareContextMenu(getContextOptions()));
             view.setClassListView(sqlClass.getKlassURLMap());
             send200(view.getResponse());
         }
